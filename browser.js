a39774d828dc6c734f2a43d648ed3735e1110d7c
naveed08es14 <155263855+naveed08es14@users.noreply.github.com>
2024-08-21 17:52:21 +0400

    Update browser.js

--- browser.js
+++ browser.js
@@ -5,7 +5,8 @@ document.addEventListener("click", function (e) {
     // Delete Feature
     if (e.target.classList.contains("delete-me")) {
         if (confirm("Do you really want to delete this item permanently?")) {
-            axios.post("/delete-item", { id: e.target.getAttribute("data-id") })
+            axios
+                .post("/delete-item", { id: e.target.getAttribute("data-id") })
                 .then(function () {
                     e.target.parentElement.parentElement.remove()
                 })
@@ -19,7 +20,8 @@ document.addEventListener("click", function (e) {
     if (e.target.classList.contains("edit-me")) {
         let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
         if (userInput) {
-            axios.post("/update-item", { text: userInput, id: e.target.getAttribute("data-id") })
+            axios
+                .post("/update-item", { text: userInput, id: e.target.getAttribute("data-id") })
                 .then(function () {
                     e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
                 })

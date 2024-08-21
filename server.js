e075d5701e3fdc0f9176b73e09f047c654fd786d
naveed08es14 <155263855+naveed08es14@users.noreply.github.com>
2024-08-21 12:06:16 +0400

    Update server.js

--- server.js
+++ server.js
@@ -75,6 +75,7 @@ app.post("/create-item", async function (req, res) {
     let safeText = sanitizeHTML(req.body.text, { allowedTags: [], allowedAttributes: {} })
     const info = await db.collection("items").insertOne({ text: req.body.item})
     res.json({ _id: info.insertedId, text: req.body.item})
+    res.send("Success")
 })
 
 app.post("/update-item", async function (req, res) {

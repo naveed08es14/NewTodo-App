e2c20926ac00660286fd1bd8555d63dda871bfa9
naveed08es14 <155263855+naveed08es14@users.noreply.github.com>
2024-08-25 23:54:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -68,13 +68,13 @@ app.get("/", passwordProtected, async function (req, res) {
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <script>
   function itemTemplate(item) {
-    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    return <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
   <span class="item-text">${item.text}</span>
   <div>
   <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
   <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
   </div>
-  </li>`
+  </li>
 }
 
 // Initial Page Load Render
